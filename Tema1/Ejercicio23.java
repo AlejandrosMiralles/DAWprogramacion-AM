7138b2240f421c38834bd45ffa278ce5ca3c8013
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-10-08 10:28:12 +0200

    Ejercicio 23, pero bien hecho

--- Tema1/Ejercicio23.java
+++ Tema1/Ejercicio23.java
@@ -2,12 +2,15 @@ import java.util.Scanner ;
 
 public class Ejercicio23 {
     public static void main(String[] args) {
-        int contador = 0, numero;
+        int contador = 0, numero, repeticiones ;
         Scanner lector = new Scanner (System.in) ;
 
-        System.out.println("Si quieres terminar el programa, escribe el número 0.") ;
+        System.out.println("Cuántos números vas a introducir? Los números negativos equivalen a 0:") ;
+        repeticiones = lector.nextInt() ;
 
-        for (int i = 0; i < 10; ++i){
+        System.out.println("Si te arrepientes y quieres terminar el programa, escribe el número 0.") ;
+
+        for (int i = 0; i < repeticiones; ++i){
 
             System.out.println("Dame un número:") ;
             numero = lector.nextInt() ;
